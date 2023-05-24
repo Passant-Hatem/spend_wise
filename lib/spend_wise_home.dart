7f39b13abdab46993a2ecc7748336627a236bc06
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2023-05-24 23:25:39 +0300

    feat: expenses list

---
+++ lib/spend_wise_home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:spend_wise/widgets/expense_item.dart';
+
+import 'models/expense.dart';
+
+class SpendWiseHome extends StatefulWidget {
+  const SpendWiseHome({super.key});
+
+  @override
+  State<SpendWiseHome> createState() {
+    return _SpendWiseHomeState();
+  }
+}
+
+class _SpendWiseHomeState extends State<SpendWiseHome> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
